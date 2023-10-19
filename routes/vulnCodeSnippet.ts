34cc773e824a8e0c90d45c9423a04d1c0779c5ce
Bugraerdeger <50201797+Bugraerdeger@users.noreply.github.com>
2023-10-19 14:48:09 +0300

    Changes 5

--- routes/vulnCodeSnippet.ts
+++ routes/vulnCodeSnippet.ts
@@ -1,16 +1,10 @@
-/*
- * Copyright (c) 2014-2023 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
- */
-
-import { type NextFunction, type Request, type Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import fs from 'fs'
 import yaml from 'js-yaml'
 import { getCodeChallenges } from '../lib/codingChallenges'
 import * as accuracy from '../lib/accuracy'
 import * as utils from '../lib/utils'
-
-const challengeUtils = require('../lib/challengeUtils')
+import challengeUtils from '../lib/challengeUtils'
 
 interface SnippetRequestBody {
   challenge: string
@@ -30,18 +24,15 @@ const setStatusCode = (error: any) => {
   }
 }
 
-export const retrieveCodeSnippet = async (challengeKey: string) => {
+const retrieveCodeSnippet = async (challengeKey: string) => {
   const codeChallenges = await getCodeChallenges()
-  if (codeChallenges.has(challengeKey)) {
-    return codeChallenges.get(challengeKey) ?? null
-  }
-  return null
+  return codeChallenges.get(challengeKey) || null
 }
 
-exports.serveCodeSnippet = () => async (req: Request<SnippetRequestBody, Record<string, unknown>, Record<string, unknown>>, res: Response, next: NextFunction) => {
+export const serveCodeSnippet = async (req: Request<SnippetRequestBody>, res: Response, next: NextFunction) => {
   try {
     const snippetData = await retrieveCodeSnippet(req.params.challenge)
-    if (snippetData == null) {
+    if (!snippetData) {
       res.status(404).json({ status: 'error', error: `No code challenge for challenge key: ${req.params.challenge}` })
       return
     }
@@ -52,18 +43,18 @@ exports.serveCodeSnippet = () => async (req: Request<SnippetRequestBody, Record<
   }
 }
 
-export const retrieveChallengesWithCodeSnippet = async () => {
+const retrieveChallengesWithCodeSnippet = async () => {
   const codeChallenges = await getCodeChallenges()
-  return [...codeChallenges.keys()]
+  return Array.from(codeChallenges.keys())
 }
 
-exports.serveChallengesWithCodeSnippet = () => async (req: Request, res: Response, next: NextFunction) => {
+export const serveChallengesWithCodeSnippet = async (req: Request, res: Response, next: NextFunction) => {
   const codingChallenges = await retrieveChallengesWithCodeSnippet()
   res.json({ challenges: codingChallenges })
 }
 
-export const getVerdict = (vulnLines: number[], neutralLines: number[], selectedLines: number[]) => {
-  if (selectedLines === undefined) return false
+const getVerdict = (vulnLines: number[], neutralLines: number[], selectedLines: number[]) => {
+  if (!selectedLines) return false
   if (vulnLines.length > selectedLines.length) return false
   if (!vulnLines.every(e => selectedLines.includes(e))) return false
   const okLines = [...vulnLines, ...neutralLines]
@@ -71,12 +62,12 @@ export const getVerdict = (vulnLines: number[], neutralLines: number[], selected
   return notOkLines.length === 0
 }
 
-exports.checkVulnLines = () => async (req: Request<Record<string, unknown>, Record<string, unknown>, VerdictRequestBody>, res: Response, next: NextFunction) => {
+export const checkVulnLines = async (req: Request<Record<string, unknown>, Record<string, unknown>, VerdictRequestBody>, res: Response, next: NextFunction) => {
   const key = req.body.key
   let snippetData
   try {
     snippetData = await retrieveCodeSnippet(key)
-    if (snippetData == null) {
+    if (!snippetData) {
       res.status(404).json({ status: 'error', error: `No code challenge for challenge key: ${key}` })
       return
     }
@@ -85,22 +76,23 @@ exports.checkVulnLines = () => async (req: Request<Record<string, unknown>, Reco
     res.status(statusCode).json({ status: 'error', error: utils.getErrorMessage(error) })
     return
   }
-  const vulnLines: number[] = snippetData.vulnLines
-  const neutralLines: number[] = snippetData.neutralLines
-  const selectedLines: number[] = req.body.selectedLines
+  const { vulnLines, neutralLines } = snippetData
+  const selectedLines = req.body.selectedLines
   const verdict = getVerdict(vulnLines, neutralLines, selectedLines)
   let hint
-  if (fs.existsSync('./data/static/codefixes/' + key + '.info.yml')) {
-    const codingChallengeInfos = yaml.load(fs.readFileSync('./data/static/codefixes/' + key + '.info.yml', 'utf8'))
+  const filePath = `./data/static/codefixes/${key}.info.yml`
+  if (fs.existsSync(filePath)) {
+    const codingChallengeInfos = yaml.load(fs.readFileSync(filePath, 'utf8'))
     if (codingChallengeInfos?.hints) {
-      if (accuracy.getFindItAttempts(key) > codingChallengeInfos.hints.length) {
+      const findItAttempts = accuracy.getFindItAttempts(key)
+      if (findItAttempts > codingChallengeInfos.hints.length) {
         if (vulnLines.length === 1) {
-          hint = res.__('Line {{vulnLine}} is responsible for this vulnerability or security flaw. Select it and submit to proceed.', { vulnLine: vulnLines[0].toString() })
+          hint = res.__(`Line ${vulnLines[0]} is responsible for this vulnerability or security flaw. Select it and submit to proceed.`)
         } else {
-          hint = res.__('Lines {{vulnLines}} are responsible for this vulnerability or security flaw. Select them and submit to proceed.', { vulnLines: vulnLines.toString() })
+          hint = res.__(`Lines ${vulnLines.join(', ')} are responsible for this vulnerability or security flaw. Select them and submit to proceed.`)
         }
       } else {
-        const nextHint = codingChallengeInfos.hints[accuracy.getFindItAttempts(key) - 1] // -1 prevents after first attempt
+        const nextHint = codingChallengeInfos.hints[findItAttempts - 1]
         if (nextHint) hint = res.__(nextHint)
       }
     }
